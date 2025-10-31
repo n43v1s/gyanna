c1f4b6ae0527d545fb3a3970086d5688fcbe9b86
n43v1s <agusrokyanto@gmail.com>
2025-10-31 12:41:13 +0700

    update form

--- Gyanna Form.py
+++ Gyanna Form.py
@@ -5,7 +5,7 @@ import time
 
 TARGET_HOUR = 12
 TARGET_MINUTE = 59
-TARGET_SECOND = 53
+TARGET_SECOND = 57
 
 print(f"Waiting until {TARGET_HOUR:02d}:{TARGET_MINUTE:02d}:{TARGET_SECOND:02d}...")
 while True:
@@ -27,17 +27,17 @@ pdob_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/di
 age_field_xpath = '//*[@id="i17"]/div[2]'
 gender1_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input'
 # gender2_field_xpath = '//*[@id="i37"]/div[2]'
-religion_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div[1]/div/div[1]/input'
-parents_name_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div[1]/div/div[1]/input'
-department_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[8]/div/div/div[2]/div/div[1]/div/div[1]/input'
-phone_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[9]/div/div/div[2]/div/div[1]/div/div[1]/input'
-address_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[10]/div/div/div[2]/div/div[1]/div/div[1]/input'
-allergies_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[11]/div/div/div[2]/div/div[1]/div/div[1]/input'
-booking_date1_field_xpath = '//*[@id="i76"]/div[2]'
-booking_date2_field_xpath = '//*[@id="i79"]/div[2]'
-booking_date3_field_xpath = '//*[@id="i82"]/div[2]'
-booking_date4_field_xpath = '//*[@id="i85"]/div[2]'
-booking_date5_field_xpath = '//*[@id="i88"]/div[2]'
+religion_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div[1]/div/div[1]/input'
+parents_name_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div[1]/div/div[1]/input'
+department_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div[1]/div/div[1]/input'
+phone_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[8]/div/div/div[2]/div/div[1]/div/div[1]/input'
+address_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[9]/div/div/div[2]/div/div[1]/div/div[1]/input'
+allergies_field_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[10]/div/div/div[2]/div/div[1]/div/div[1]/input'
+booking_date1_field_xpath = '//*[@id="i71"]/div[2]'
+booking_date2_field_xpath = '//*[@id="i74"]/div[2]'
+booking_date3_field_xpath = '//*[@id="i77"]/div[2]'
+booking_date4_field_xpath = '//*[@id="i80"]/div[2]'
+booking_date5_field_xpath = '//*[@id="i83"]/div[2]'
 submit_button_xpath = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span'
 
 # texts
@@ -53,52 +53,53 @@ allergies = "None"
 
 driver = webdriver.Chrome()
 
-driver.get("https://docs.google.com/forms/d/e/1FAIpQLSfAroUfYkZqG7MuHRSG2-ly81tfK4qbw0frwnjhZP-hmgJQqQ/viewform")
+driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdWVUCFpICwmeqUKVPNhtc_w9HryyeZ_iGcUeGZEoivdiJilQ/viewform")
 
 time.sleep(1)
 
 childs_name_field = driver.find_element(By.XPATH, childs_name_field_xpath)
 childs_name_field.send_keys(childs_name)
-#time.sleep(2)
+# time.sleep(1.2)
 
 pdob_field = driver.find_element(By.XPATH, pdob_field_xpath)
 pdob_field.send_keys(pdob)
-#time.sleep(2)
+# time.sleep(1.2)
 
 age_field = driver.find_element(By.XPATH, age_field_xpath)
 age_field.click()
-#time.sleep(2)
+# time.sleep(1.2)
 
-pdob_field = driver.find_element(By.XPATH, gender1_field_xpath)
-pdob_field.send_keys(gender)
-#time.sleep(2)
+# pdob_field = driver.find_element(By.XPATH, gender1_field_xpath)
+# pdob_field.send_keys(gender)
+# # time.sleep(1.2)
 
 # gender_field = driver.find_element(By.XPATH, gender2_field_xpath)
 # gender_field.click()
+# # time.sleep(1.2)
 
 religion_field = driver.find_element(By.XPATH, religion_field_xpath)
 religion_field.send_keys(religion)
-#time.sleep(2)
+# time.sleep(1.2)
 
 parents_name_field = driver.find_element(By.XPATH, parents_name_field_xpath)
 parents_name_field.send_keys(parents_name)
-#time.sleep(2)
+# time.sleep(1.2)
 
 department_field = driver.find_element(By.XPATH, department_field_xpath)
 department_field.send_keys(department)
-#time.sleep(2)
+# time.sleep(1.2)
 
 phone_field = driver.find_element(By.XPATH, phone_field_xpath)
 phone_field.send_keys(phone)
-#time.sleep(2)
+# time.sleep(1.2)
 
 address_field = driver.find_element(By.XPATH, address_field_xpath)
 address_field.send_keys(address)
-#time.sleep(2)
+# time.sleep(1.2)
 
 allergies_field = driver.find_element(By.XPATH, allergies_field_xpath)
 allergies_field.send_keys(allergies)
-#time.sleep(2)
+# time.sleep(1.2)
 
 
 booking_date1_field = driver.find_element(By.XPATH, booking_date1_field_xpath)
@@ -108,25 +109,25 @@ booking_date4_field = driver.find_element(By.XPATH, booking_date4_field_xpath)
 booking_date5_field = driver.find_element(By.XPATH, booking_date5_field_xpath)
 
 booking_date1_field.click()
-#time.sleep(2)
+# time.sleep(1.2)
 
 booking_date2_field.click()
-#time.sleep(2)
+# time.sleep(1.2)
 
 booking_date3_field.click()
-#time.sleep(2)
+# time.sleep(1.2)
 
 booking_date4_field.click()
-#time.sleep(2)
+# time.sleep(1.2)
 
 booking_date5_field.click()
-#time.sleep(2)
+# time.sleep(1.2)
 
 submit_button = driver.find_element(By.XPATH, submit_button_xpath)
-submit_button.click()
+# submit_button.click()
 end_time = time.time()
 
-time.sleep(2)
+# time.sleep(1.2)
 
 # screenshot = pyautogui.screenshot()
 # filename = datetime.now().strftime("screenshot_%Y%m%d_%H%M%S.png")
@@ -136,7 +137,7 @@ time.sleep(2)
 elapsed_time = end_time - start_time  # seconds taken
 
 print(f"Total time taken: {elapsed_time:.2f} seconds")
-print("Form Submmited on " + datetime.now().strftime("%m/%d/%Y %I:%M %p"))
+print("Form Submmited on " + datetime.now().strftime("%m/%d/%Y %I:%M:%S %p"))
 
 time.sleep(3600)
 # driver.quit()
